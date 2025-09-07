27821195372c35892e6d079dc8e9c8594c3d9476
Adamyan Tigran <tigran05012002@mail.ru>
2025-09-07 11:11:18 +0300

    Убрал лого

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -21,13 +21,13 @@ const showingNavigationDropdown = ref(false);
                     <div class="flex h-16 justify-between">
                         <div class="flex">
                             <!-- Logo -->
-                            <div class="flex shrink-0 items-center">
+                           <!-- <div class="flex shrink-0 items-center">
                                 <Link :href="route('dashboard')">
                                     <ApplicationLogo
                                         class="block h-9 w-auto fill-current text-gray-800"
                                     />
                                 </Link>
-                            </div>
+                            </div>-->
 
                             <!-- Navigation Links -->
                             <div
